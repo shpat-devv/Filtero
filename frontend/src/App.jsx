38db201bb5995027afc798ca1c906d3890b471bf
Shpat Avdiu <uglyprincess@Shpats-MacBook-Pro.fritz.box>
2025-10-10 14:45:14 +0200

    base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ProtectedRoute from './components/ProtectedRoute'
+
+//styles
+import './styles/root.css'
+import './styles/general.css'
+
+function logout() {
+  localStorage.clear; //TODO: Clear only the auth token
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear;
+  return ( <Register />)
+}
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProtectedRoute><Camera /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<logout />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+  )
+}
+export default App
